44a2d10c53c4c2c8f557dffd510b65d728fcfc2d
taylorgrohs <taylorgrohs@gmail.com>
2016-05-10 08:38:37 -0700

    test project

---
+++ TwilioTest/TwilioTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace TwilioTest
+{
+    class Program
+    {
+        public class Message
+        {
+            public string To { get; set; }
+            public string From { get; set; }
+            public string Body { get; set; }
+            public string Status { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            //1
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            //2
+            var request = new RestRequest("Accounts/AC511da8b64e69517082a8911962dfab8c/Messages.json", Method.GET);
+
+            //4
+            client.Authenticator = new HttpBasicAuthenticator("AC511da8b64e69517082a8911962dfab8c", "b07f4c2a9aaeac2f4d1feee0643dd25b");
+            //5
+            var response = client.Execute(request);
+            JObject jsonResponse = (JObject)JsonConvert.DeserializeObject(response.Content);
+            var messageList = JsonConvert.DeserializeObject<List<Message>>(jsonResponse["messages"].ToString());
+            foreach (var message in messageList)
+            {
+                Console.WriteLine("To: {0}", message.To);
+                Console.WriteLine("From: {0}", message.From);
+                Console.WriteLine("Body: {0}", message.Body);
+                Console.WriteLine("Status: {0}", message.Status);
+            }
+            Console.ReadLine();
+        }
+    }
+}
